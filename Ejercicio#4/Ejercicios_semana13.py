5f299feb6aa7c228b0dfb068dbd11927b9c4d30a
JulianClotCordoba <jclot60815@ufide.ac.cr>
2024-04-12 12:29:03 -0600

    Ejercicio #4

--- Ejercicio#4/Ejercicios_semana13.py
+++ Ejercicio#4/Ejercicios_semana13.py
@@ -1,14 +1,42 @@
 
+with open("personas.txt", "w") as personas_data:
+    pass
 
-personas_data = open("personas.txt", "w")
+personas_ing = []
+personas_eg = []
 
 def ingresar():
-    for i in range(1, 11):
-        cantidad_de_personas = input("Cual es tu nombre para ingresarte? ")
-
-        personas_data.write( "\n" +cantidad_de_personas + "\n")
-    personas_data.close()
+    cantidad_de_personas_ingresadas = input("Cual es tu nombre para ingresarte ")
+    personas_ing.append(cantidad_de_personas_ingresadas)
+    if len(personas_ing) <= 10:
+        with open("personas.txt", "a") as personas_data:
+            personas_data.write("\n" + cantidad_de_personas_ingresadas + "\n")
+    else:
+        print("La cantidad de personas supero el maximo de capacidad.")
 
 def egresar():
-    for x in range(1, 11):
-        cantidad_de_personas_egresadas = input("Cual es tu nombre para egresarte? ")
+    cantidad_de_personas_egresadas = input("Cual es tu nombre para egresarte ")
+    if cantidad_de_personas_egresadas in personas_ing:
+        personas_ing.remove(cantidad_de_personas_egresadas)
+        with open("personas.txt", 'r+') as per:
+            contenido = per.read()
+            contenido_modificado = contenido.replace(cantidad_de_personas_egresadas, '')
+            per.seek(0)
+            per.write(contenido_modificado)
+            per.truncate()
+        print(f"{cantidad_de_personas_egresadas} ha salido correctamente.")
+    else:
+        print(f"{cantidad_de_personas_egresadas} no se encuentra en la lista de ingresados.")
+
+while True:
+    print("1. Ingresar")
+    print("2. Egresar")
+    print("3. Salir")
+    opcion = input("Que opciion quieres elegir ")
+
+    if opcion == "1":
+        ingresar()
+    elif opcion == "2":
+        egresar()
+    elif opcion == "3":
+        break
